2870a2138ee52c77ddea13c344c3b7f5c06e163f
Changmin Bae <65561206+baec23@users.noreply.github.com>
2022-05-26 13:57:47 +0900

    Login using JWT -authToken and refreshToken

--- src/main/java/com/baec/texttwist/TexttwistApplication.java
+++ src/main/java/com/baec/texttwist/TexttwistApplication.java
@@ -1,9 +1,17 @@
 package com.baec.texttwist;
 
+import com.baec.texttwist.model.AppUser;
+import com.baec.texttwist.model.Role;
 import com.baec.texttwist.service.UserService;
+import org.apache.catalina.User;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class TexttwistApplication
@@ -14,11 +22,25 @@ public class TexttwistApplication
         SpringApplication.run(TexttwistApplication.class, args);
     }
 
-    CommandLineRunner run(UserService userService)
+    @Bean
+    PasswordEncoder passwordEncoder()
     {
-        return args -> {
-          userService.saveRole(new Role())
-        };
+        return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    CommandLineRunner run(UserService userService)
+    {
+        return args ->
+        {
+            userService.saveRole(new Role(null, "ROLE_USER"));
+            userService.saveRole(new Role(null, "ROLE_MANAGER"));
+            userService.saveRole(new Role(null, "ROLE_ADMIN"));
+            userService.saveRole(new Role(null, "ROLE_SUPER_ADMIN"));
+
+            userService.saveUser(new AppUser(null, "Test User", "testuser1", "testtest", new ArrayList<>()));
+
+            userService.addRoleToUser("testuser1", "ROLE_USER");
+        };
+    }
 }
--- src/main/java/com/baec/texttwist/controller/UserController.java
+++ src/main/java/com/baec/texttwist/controller/UserController.java
@@ -1,17 +1,37 @@
 package com.baec.texttwist.controller;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.baec.texttwist.model.AppUser;
 import com.baec.texttwist.model.Role;
 import com.baec.texttwist.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.net.URI;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
+import static java.util.Arrays.stream;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@Slf4j
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
@@ -44,6 +64,48 @@ public class UserController
     {
         userService.addRoleToUser(form.getUsername(), form.getRoleName());
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/token/refresh")
+    public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException
+    {
+        String authorizationHeader = request.getHeader(AUTHORIZATION);
+        if(authorizationHeader != null && authorizationHeader.startsWith("Bearer "))
+        {
+            try
+            {
+                String refreshToken = authorizationHeader.substring("Bearer ".length());
+                Algorithm alg = Algorithm.HMAC256("secret".getBytes());
+                JWTVerifier verifier = JWT.require(alg).build();
+                DecodedJWT decodedToken = verifier.verify(refreshToken);
+                String username = decodedToken.getSubject();
+                AppUser user = userService.getUser(username);
+                String accessToken = JWT.create()
+                        .withSubject(user.getUsername())
+                        .withExpiresAt(new Date(System.currentTimeMillis() + 10 * 60 * 1000))
+                        .withIssuer(request.getRequestURL().toString())
+                        .withClaim("roles", user.getRoles().stream().map(Role::getName).collect(Collectors.toList()))
+                        .sign(alg);
+                Map<String, String> tokens = new HashMap<>();
+                tokens.put("accessToken", accessToken);
+                tokens.put("refreshToken", refreshToken);
+                new ObjectMapper().writeValue(response.getOutputStream(), tokens);
+            }
+            catch(Exception e)
+            {
+                log.error("Error logging in: {}", e.getMessage());
+                response.setHeader("error", e.getMessage());
+                response.setStatus(FORBIDDEN.value());
+                Map<String, String> error = new HashMap<>();
+                error.put("errorMessage", e.getMessage());
+                response.setContentType(APPLICATION_JSON_VALUE);
+                new ObjectMapper().writeValue(response.getOutputStream(), error);
+            }
+        }
+        else
+        {
+            throw new RuntimeException("Refresh token error");
+        }
     }
 }
 @Data
